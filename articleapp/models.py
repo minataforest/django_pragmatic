f98b3d3c695ae91bba973a02a968c361a9d998e8
minataforest <minataforest@gmail.com>
2022-03-23 20:37:45 +0900

    220323

---
+++ articleapp/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Article(models.Model):
+    # on_delete=models.SET_NULL: 회원탈퇴 시, 게시글 지워지지 않고 주인 없는 게시글이 됨
+    # related_name은 User 객체에서 접근하기 위한 이름이므로 article로 한다
+    writer = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="article", null=True
+    )
+
+    title = models.CharField(max_length=100, null=True)
+    image = models.ImageField(upload_to="article/", null=False)
+    content = models.TextField(null=True)
+
+    # auto_now_add=True 생성시간 자동생성
+    created_at = models.DateField(auto_now_add=True, null=True)
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -10,6 +12,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = "target_profile"
     form_class = ProfileCreationForm
+    # success_url에서는 동적인 추가데이터를 보내줄 수 없다.
+    # 그러므로 내부 메서드를 수정한다.
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = "profileapp/create.html"
 
@@ -18,3 +22,21 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
 
         return super().form_valid(form)
+
+    def get_success_url(self):
+        # 여기서 self는 Profile임
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
+
+
+@method_decorator(profile_ownership_required, "get")
+@method_decorator(profile_ownership_required, "post")
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = "target_profile"
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "profileapp/update.html"
+
+    def get_success_url(self):
+        # 여기서 self는 Profile임
+        return reverse("accountapp:detail", kwargs={"pk": self.object.user.pk})
